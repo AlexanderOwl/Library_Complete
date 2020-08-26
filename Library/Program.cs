7a85e20be034aa377806d42f57796bb072f51ac4
Олександр Owl <ischenk1756@yandex.ua>
2020-08-26 11:31:48 +0300

    code style fix

--- Library/Program.cs
+++ Library/Program.cs
@@ -6,23 +6,23 @@ namespace Library
 {
     class Program
     {
-        static List<Book> _books;
+        
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
             Console.InputEncoding = Encoding.Unicode;
-
+            List<Book> books;
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Welcome to Console Library V.1 \nInput help to list of command \n");
             Console.ResetColor();
 
-            _books = !File.Exists("lib.xml") ? new List<Book>() : Menu.DeserializeFromXML(); // Если существует файл читает из него, если файла нет создает новый список книг.
+            books = !File.Exists("lib.xml") ? new List<Book>() : Menu.DeserializeFromXML(); // Если существует файл читает из него, если файла нет создает новый список книг.
             Menu menu = new Menu(); //Экземпляр класса меню.
             if (!File.Exists("lib.xml"))
             {
-                menu.Add5(_books);
+                menu.Add5(books);
             }
-            menu.Open(_books);// Вызов метода Open класса меню.
+            menu.Open(books);// Вызов метода Open класса меню.
         }
     }
 }
